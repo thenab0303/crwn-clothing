dd4a63267b3e99c3af3c130ba2897517df8803de
TheNab <thenab0303@gmail.com>
2021-07-24 05:15:25 -0400

    initial react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import { HomePage } from './Components/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
