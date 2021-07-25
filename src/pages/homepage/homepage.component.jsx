a79e65f75d049ea68ecbc845adc1a7ecaf23a61f
TheNab <thenab0303@gmail.com>
2021-07-25 10:27:17 -0400

    broken homepage into smaller components and finished styling menu-item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+
+import Directory from '../../Components/directory/directory.components'
+import './homepage.styles.scss'
+
+export const HomePage = () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+)
