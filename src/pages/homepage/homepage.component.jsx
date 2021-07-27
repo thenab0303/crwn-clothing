a91b3f5ea7d1b3d7d1b2474bf427eb2da1150862
TheNab <thenab0303@gmail.com>
2021-07-27 12:18:25 -0400

    updating app with shop page, collection preview component and collection item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,11 @@
 
+import { Link } from 'react-router-dom'
 import Directory from '../../Components/directory/directory.components'
 import './homepage.styles.scss'
 
 export const HomePage = () => (
     <div className='homepage'>
+        <Link to='/shop'>Shop Page</Link>
         <Directory/>
     </div>
 )
